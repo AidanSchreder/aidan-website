88c54e83415389cd3c6f564be319902c0ec74741
Aidan Schreder <aidan.schreder@gmail.com>
2026-02-24 16:04:17 -0500

    Fixed resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -9,12 +9,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Container>
-      <span className="text-4xl">💼</span>
       <Heading className="font-black">Work History</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        I&apos;m an engineer and designer that loves{" "}
+        <Highlight>building stuff</Highlight> that can impact
+        people
       </Paragraph>
       <WorkHistory />
     </Container>
--- src/constants/timeline.tsx
+++ src/constants/timeline.tsx
@@ -1,62 +1,29 @@
 export const timeline = [
   {
-    company: "Amazon",
-    title: "Senior Software Engineer",
-    date: "2022 - Present",
-    description: "Building the future of e-commerce",
+    company: "National Research Council Canada",
+    title: "Research Assistant, Human Swarm Teaming",
+    date: "2026 - Present",
+    description: "Building with cutting edge robotics, AI and control systems",
     responsibilities: [
-      "Developed a new feature that improves the shopping experience",
-      "Implemented a recommendation algorithm to suggest products",
-      "Enhanced the checkout process making it more intuitive and user-friendly",
-      "Built a feature for tracking packages in real-time",
+      
     ],
   },
   {
-    company: "Google",
-    title: "Senior Software Engineer",
-    date: "2021 - 2022",
-    description: "Building the future of search",
+    company: "Magical",
+    title: "Verfication agent",
+    date: "2025",
+    description: "Verifying and improving agentic agentic AI systems",
     responsibilities: [
-      "Improved the search algorithm to provide more relevant results",
-      "Developed a new feature that personalizes search results",
-      "Enhanced the search functionality making it more intuitive and user-friendly",
-      "Implemented a feature for tracking search history",
+      
     ],
   },
   {
-    company: "Facebook",
-    title: "Senior Software Engineer",
-    date: "2020 - 2021",
-    description: "Building the future of social networking",
+    company: "City of Toronto",
+    title: "Skate Instructor, Community Service Representative",
+    date: "2020 - 2025",
+    description: "Skate instruction, public service, and more",
     responsibilities: [
-      "Developed a new feature that improves the user experience",
-      "Implemented a recommendation algorithm to suggest friends",
-      "Enhanced the news feed algorithm to show more relevant posts",
-      "Built a feature for tracking user activity",
-    ],
-  },
-  {
-    company: "Apple",
-    title: "Senior Software Engineer",
-    date: "2019 - 2020",
-    description: "Building the future of technology",
-    responsibilities: [
-      "Improved the iOS operating system to enhance user experience",
-      "Developed a new feature that personalizes user interface",
-      "Enhanced the app store algorithm to suggest more relevant apps",
-      "Implemented a feature for tracking app usage",
-    ],
-  },
-  {
-    company: "Microsoft",
-    title: "Senior Software Engineer",
-    date: "2018 - 2019",
-    description: "Building the future of software",
-    responsibilities: [
-      "Improved the Windows operating system to enhance user experience",
-      "Developed a new feature that personalizes user interface",
-      "Enhanced the office suite to improve productivity",
-      "Implemented a feature for tracking software usage",
+
     ],
   },
 ];
