f4484fda1f730d2781ad419e612575dfb932c923
Aidan Schreder <aidan.schreder@gmail.com>
2026-02-25 19:55:30 -0500

    Fully fixed Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,12 +45,12 @@ export default function RootLayout({
         />
 
         <Script id="google-analytics" strategy="afterInteractive">
-            {'
+            {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-3RYJ5FDDWX');
-            '}
+            `}
         </Script>
 
       </body>
